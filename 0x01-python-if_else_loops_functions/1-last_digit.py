2bdab522b0cab4257e0bcc852c90bfdb3e8e713b
asimsharf <asimsharf@gmail.com>
2023-06-06 22:12:34 +0300

    Last digit of, and is greater than

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
+msg1 = "Last digit of"
+msg2 = "and is greater than 5"
+msg3 = "and is less than 6 and not 0"
+if number < 0:
+    last = number % -10
+else:
+    last = number % 10
+if last > 5:
+    print("{:s} {:d} is {:d} {:s}".format(msg1, number, last, msg2))
+elif last == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, last))
+else:
+    print("{:s} {:d} is {:d} {:s}".format(msg1, number, last, msg3))
