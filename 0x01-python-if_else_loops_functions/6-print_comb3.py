ba43121d96ce333564504c4003c66627b3431ff1
asimsharf <asimsharf@gmail.com>
2023-06-07 19:36:12 +0300

    update output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 99:
+for i in range(1, 90):
+    if i != 89:
         print("{:02d}, ".format(i), end="")
     else:
         print("{:02d}".format(i))
