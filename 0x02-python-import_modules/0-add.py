a9a60c606e6e9deb7ec1835415f6dea204f5fd64
asimsharf <asimsharf@gmail.com>
2023-06-12 19:42:57 +0300

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__":
+    ''' print sum of a and b '''
+from add_0 import add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
