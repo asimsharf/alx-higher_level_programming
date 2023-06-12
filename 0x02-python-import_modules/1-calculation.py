335e511f94aef887cfb2bc4e9745d052df566012
asimsharf <asimsharf@gmail.com>
2023-06-12 19:45:33 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    ''' print sum of a and b '''
+    ''' print sum sub mul div from module'''
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{0:d} + {1:d} = {2:d}\n{0:d} - {1:d} = {3:d}".format(
+      a, b, add(a, b), sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
