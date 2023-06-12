6af4dde9becad0eee0f3230d6cbc57354d997dff
asimsharf <asimsharf@gmail.com>
2023-06-12 22:46:25 +0300

    update

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def switch(a_dictionary):
-    new_dictionary = {}
-    for key, value in a_dictionary.items():
-        new_dictionary[value] = key
-    return new_dictionary
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
