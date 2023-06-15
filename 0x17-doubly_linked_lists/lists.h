d9194683fb3606ad3e5c97991d7dedbc299c0940
asimsharf <asimsharf@gmail.com>
2023-06-15 23:05:41 +0300

    Added functions for double linked list manipulation: print, add, insert, delete.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,34 +1,59 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Libraries */
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include "lists.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+/* Structures */
 
 /**
- * main - check the code
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Return: Always EXIT_SUCCESS.
+ * Description: doubly linked list node structure
+ *
  */
-int main(void)
+typedef struct dlistint_s
 {
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = print_dlistint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
-}
+/* Prototypes */
+
+/* 0. Print list */
+size_t print_dlistint(const dlistint_t *h);
+
+/* 1. List length */
+size_t dlistint_len(const dlistint_t *h);
+
+/* 2. Add node */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+/* 3. Add node at the end */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
+/* 4. Free list */
+void free_dlistint(dlistint_t *head);
+
+/* 5. Get node at index */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+/* 6. Sum list */
+int sum_dlistint(dlistint_t *head);
+
+/* 7. Insert at index */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+
+/* 8. Delete at index */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif /* LISTS_H */
