08f7dc6d89d20dafff49d35e9621aa7bf9e4bc85
asimsharf <asimsharf@gmail.com>
2023-06-28 03:21:47 +0300

    Write a class Square that defines a square by: (based on 3-square.py)

--- 0x06-python-classes/run.sh
+++ 0x06-python-classes/run.sh
@@ -3,3 +3,18 @@ python3 -c 'print(__import__("0-square").Square.__doc__)'
 
 python3 -c 'print(__import__("1-square").__doc__)'
 python3 -c 'print(__import__("1-square").Square.__doc__)'
+
+python3 -c 'print(__import__("2-square").__doc__)'
+python3 -c 'print(__import__("2-square").Square.__doc__)'
+
+python3 -c 'print(__import__("3-square").__doc__)'
+python3 -c 'print(__import__("3-square").Square.__doc__)'
+
+python3 -c 'print(__import__("4-square").__doc__)'
+python3 -c 'print(__import__("4-square").Square.__doc__)'
+
+python3 -c 'print(__import__("5-square").__doc__)'
+python3 -c 'print(__import__("5-square").Square.__doc__)'
+
+python3 -c 'print(__import__("6-square").__doc__)'
+python3 -c 'print(__import__("6-square").Square.__doc__)'
